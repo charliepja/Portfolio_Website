893cfdc3b34da3f116ad8e476c789a88231657cf
Charlie <19439935+charliepja@users.noreply.github.com>
2020-06-03 21:34:00 +0100

    adds navbar with pfp, bio, and links

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AppNavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -6,7 +7,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>Hello World</h1>
+			<AppNavBar />
 			</div>
 		);
 	}
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
 import {
-	Collapse,
-	NavBar,
-	NavBarToggler,
-	NavBarBrand,
 	Nav,
 	NavItem,
-	NavLink,
-	Container
+	NavLink
 } from 'reactstrap';
 
-class NavBar extends Component {
+class AppNavBar extends Component {
 	constructor(props) {
 		super(props);
+	}
 
-		this.toggle = this.toggle.bind(this);
+	render() {
+		return (
+			<div className="nav text-center">
+				<div>
+					<img src="https://i.imgur.com/TAmWSEW.gif" className="navImg rounded-circle mx-auto d-block" alt="picture of person" />
+				</div>
+				<div className= "text-center navBio">
+					<p>Hi! I'm Charlie, and I'm a developer!</p>
+				</div>
+				<Nav className="mr-auto" vertical>
+					<NavItem>
+						<NavLink href="/" className="navText">
+							Home
+						</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink href="/" className="navText">
+							Github
+						</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink href="/" className="navText">
+							LinkedIn
+						</NavLink>
+					</NavItem>
+				</Nav>
+			</div>
+		)
 	}
 }
 
-export default NavBar;
+export default AppNavBar;
