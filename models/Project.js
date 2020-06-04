e2e9efa069c267a39919aa82f29435e71639b12c
Charlie <19439935+charliepja@users.noreply.github.com>
2020-06-04 14:00:37 +0100

    creates mongoose schema and model

--- models/Project.js
+++ models/Project.js
@@ -1,1 +1,21 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Mongoose Scheme
+
+const ProjectSchema = new Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	languages: {
+		type: Array,
+		required: true
+	},
+	website: {
+		type: String,
+		required: true
+	}
+});
+
+module.exports = Project = mongoose.model('project', ProjectSchema);
