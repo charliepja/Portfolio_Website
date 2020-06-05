3c51e0fe101bff66e5d241a068ef71d05f3ff989
Charlie <19439935+charliepja@users.noreply.github.com>
2020-06-05 12:45:10 +0100

    removes post and delete route

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -14,32 +14,4 @@ router.get('/', (req, res) => {
 		.then(projects => res.json(projects))
 });
 
-// @route POST api/projects
-// @desc CREATE a Project
-// @access Public
-
-router.post('/', (req, res) => {
-	const newProject = new Project({
-		name: req.body.name,
-		languages: req.body.languages,
-		website: req.body.website,
-		github: req.body.github,
-		img: req.body.img,
-		description: req.body.description
-	});
-
-	newProject.save()
-		.then(project => res.json(project));
-});
-
-// @route DELETE api/projects/:id
-// @desc DELETE a Project
-// @access Public
-
-router.delete('/:id', (req, res) => {
-	Project.findById(req.params.id)
-		.then(project => project.remove().then(() => res.json({ success: true })))
-		.catch(err => res.status(404).json({ success: false }));
-});
-
 module.exports = router;
