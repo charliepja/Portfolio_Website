1303e5488b64e76a5da3aa7b47a43d0e5a9e8073
Charlie <19439935+charliepja@users.noreply.github.com>
2020-07-30 13:37:13 -0700

    navbar fix

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -27,7 +27,7 @@ class AppNavBar extends Component {
 					* Current Project<br />
 					</p>
 				</div>
-				<Nav className="mr-auto navbar-expand" vertical>
+				<Nav className="mr-auto nBar" vertical>
 					<NavItem>
 						<NavLink href="/" className="navText">
 							Home
